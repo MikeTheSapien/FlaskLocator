cee976623a093298427067fb66783ed3f50a112b
Mike Dula <mikedula368@gmail.com>
2020-04-28 11:31:47 +0800

    created a directory for auxiliary tools for the app

---
+++ Utils/Initializer.py
@@ -0,0 +1,3 @@
+# @MikeTheSapien
+# accountant@leboucher.ph
+# this file would hold a class for reading a .ini file for constant values: email recipient, email sender, email account password
---
+++ Utils/Mailer.py
@@ -0,0 +1,3 @@
+# @MikeTheSapien
+# accountant@leboucher.ph
+# this file would hold a class for sending logs thru email
